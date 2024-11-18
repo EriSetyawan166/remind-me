afc2518216e8faf266e3f3fc09ba51dbd12448eb
Muhammad Eri Setyawan <erisetyawan34@gmail.com>
2024-11-18 09:54:07 +0700

    feat(dashboard): add initial collection list view

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,19 +1,26 @@
+import CreateCollectionBtn from "@/components/CreateCollectionBtn";
+import SadFace from "@/components/icons/SadFace";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
+import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <>
-      <Suspense fallback={ <WelcomeMsgFallback /> }>
+      <Suspense fallback={<WelcomeMsgFallback />}>
         <WelcomeMsg />
+      </Suspense>
+      <Suspense fallback={<div>Loading Collections...</div>}>
+        <CollectionList />
       </Suspense>
     </>
   )
 }
 
 async function WelcomeMsg() {
-  const user = await currentUser(); 
+  const user = await currentUser();
 
   if (!user) {
     return <div>error</div>
@@ -21,9 +28,9 @@ async function WelcomeMsg() {
 
 
   return (
-    <div className="flex w-full">
+    <div className="flex w-full mb-12">
       <div className="text-4xl font-bold">
-        <div>Welcome, <br /> {user.firstName} { user.lastName }</div>
+        <div>Welcome, <br /> {user.firstName} {user.lastName}</div>
       </div>
     </div>
   );
@@ -31,11 +38,33 @@ async function WelcomeMsg() {
 
 function WelcomeMsgFallback() {
   return (
-    <div className="flex w-full">
+    <div className="flex w-full mb-12">
       <div className="text-4xl font-bold">
-        <Skeleton className="w-[180px] h-[36px]"/>
-        <Skeleton className="w-[180px] h-[36px]"/>
+        <Skeleton className="w-[180px] h-[36px]" />
+        <Skeleton className="w-[180px] h-[36px]" />
       </div>
     </div>
   )
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collections = await prisma.collection.findMany({
+    where: {
+      userId: user?.id,
+    }
+  });
+
+  if (collections.length === 0) {
+    return (
+      <div className="flex flex-col gap-5">
+        <Alert>
+          <SadFace />
+          <AlertTitle>There are no collection yet!</AlertTitle>
+          <AlertDescription>Create a collection to get started</AlertDescription>
+        </Alert>
+        <CreateCollectionBtn />
+      </div>
+    );
+  }
 }
---
+++ components/CreateCollectionSheet.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from './ui/sheet';
+
+interface Props {
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+}
+
+function CreateCollectionSheet({ open, onOpenChange }: Props) {
+    return (
+        <Sheet open={ open } onOpenChange={onOpenChange}>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Add a new collection</SheetTitle>
+                    <SheetDescription>Collections are a way to group your tasks.</SheetDescription>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+    )
+}
+
+export default CreateCollectionSheet
