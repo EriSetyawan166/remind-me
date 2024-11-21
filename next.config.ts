69ee4a408eaad4723e42df9d669c01c5dda72c6b
Muhammad Eri Setyawan <erisetyawan34@gmail.com>
2024-11-21 10:31:53 +0700

    chore: disable react strict mode

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  reactStrictMode: false,
   experimental: {
     serverActions: {
       bodySizeLimit: "1mb",
